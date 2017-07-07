6884ef4bcf10aa43c08cd2711294fc3c0bfaa664
Devanamapriyan <Devanamapriyan@users.noreply.github.com>
2017-07-07 12:30:47 +0530

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -2,4 +2,5 @@ name = "Joe"
 feet= 6
 inches= 2
 
-print("My Name is <any name>. My Height is : <any height> feet and inches long")
+print("My Name is <any name>. My Height is : <any height> feet and inches long") # make use of the 2 variables and create a comma seperated print statement
+print(myvar) # create a myvar variable to hold the completed string as above and use it for this print
