9e0817b18a30dee9748072be1c964cca758183eb
Devanamapriyan <joeljose420@gmail.com>
2017-07-14 11:18:55 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,5 +1,6 @@
 food={"vegetables":["carrots","kale","cucumber","tomato"],"desserts":["cake","ice cream", "donut"]}
 
+<<<<<<< HEAD
 cars={"sports":{"Volkswagon":"Porsche","Dodge":"Viper","Chevy":"Corvette"},"classic":{"Mercedes-Benz":"300SL","Toyota":"2000GT","Lincoln":"Continental"}}
 
 
@@ -7,3 +8,7 @@ print("The vegetables are:")
 print("The deserts are:")
 print("The sports cars are:")
 print("The classic cars are:")
+=======
+print("My Name is <any name>. My Height is : <any height> feet and inches long") # make use of the 2 variables and create a comma seperated print statement
+print(myvar) # create a myvar variable to hold the completed string as above and use it for this print
+>>>>>>> origin/master
